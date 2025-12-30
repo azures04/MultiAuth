5945ea3f95984fb7a40bfb9f48b575d6e5d14d32
azures04 <gilleslazure04@gmail.com>
2025-12-30 09:11:49 +0100

    First commit

---
+++ src/main/java/fr/azures04/mods/multiauth/MultiAuth.java
@@ -0,0 +1,23 @@
+package fr.azures04.mods.multiauth;
+
+import org.apache.logging.log4j.Logger;
+import fr.azures04.mods.multiauth.utils.Constants;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Constants.MODID, name =Constants.NAME, version = Constants.VERSION)
+public class MultiAuth {
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+    }
+}
---
+++ src/main/java/fr/azures04/mods/multiauth/utils/Constants.java
@@ -0,0 +1,9 @@
+package fr.azures04.mods.multiauth.utils;
+
+public class Constants {
+
+    public static final String MODID = "multiauth";
+    public static final String NAME = "MultiAuth";
+    public static final String VERSION = "0.0.1";
+	
+}
